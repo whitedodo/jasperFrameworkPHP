a6120e2bb56639a6c183d3cfbecfff5a608ea1f4
Rabbitwhite <34395602+whitedodo@users.noreply.github.com>
2018-08-11 18:18:44 +0900

    Add files via upload

--- view/common/w3cLogo.php
+++ view/common/w3cLogo.php
@@ -5,12 +5,12 @@
         <a href="http://jigsaw.w3.org/css-validator/check/referer">
             <img style="border:0;width:88px;height:31px"
                 src="//jigsaw.w3.org/css-validator/images/vcss"
-                alt="�ùٸ� CSS�Դϴ�!" />
+                alt="올바른 CSS입니다!" />
         </a>
         <a href="http://jigsaw.w3.org/css-validator/check/referer">
             <img style="border:0;width:88px;height:31px"
                 src="//jigsaw.w3.org/css-validator/images/vcss-blue"
-                alt="�ùٸ� CSS�Դϴ�!" />
+                alt="올바른 CSS입니다!" />
         </a>
         </td>
 	</tr>
