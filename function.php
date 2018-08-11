92b9b64d8db9061c23ef89f328d393fe33b9acab
Rabbitwhite <34395602+whitedodo@users.noreply.github.com>
2018-08-11 16:41:10 +0900

    Add files via upload

--- function.php
+++ function.php
@@ -5,13 +5,163 @@
  * Created by 2018-08-07
  * Author: Dodo (rabbit.white at daum dot net)
  * Description:
- * 2018-08-10 / Jasper / csvToViewer($file) 기능 추가
+ * 2018-08-09 / Jasper / csvToViewer($file) 기능 추가
  */
 
 class JasperFunction{
     
     private $bookMin = 18;
     private $bookMax = 21;
+    
+    private $houseMin = 14;
+    private $houseMax = 16;
+    
+    // CSV To Viewer(House)
+    function csvToHousePay($file){
+        
+        ini_set("allow_url_fopen", 0);
+        
+        $row = 1;
+        
+        if (($handle = fopen("$file", "r")) !== FALSE) {
+            
+            echo "<table class=\"tg_general\">";
+            
+            $index = 0;
+            $arrHeader = array();
+            $arrTitle = array();
+            $arrData = array();
+            $arrCount = -1;
+            
+            while (($data = fgetcsv($handle, 4096, ",")) !== FALSE) {
+                
+                $num = count($data);
+                
+                // Title 출력
+                if ( $index > 1 ){
+                    
+                    echo "\t<tr>\n";
+                    foreach ( $arrHeader as $arrVal ){
+                        
+                        echo "<th>";
+                        echo "<b>$arrVal</b>";
+                        echo "</th>";
+                    }
+                    echo "\t</tr>\n";
+                }
+                
+                echo "\t<tr>\n";
+                
+                for ($row = 0; $row < $num; $row++) {
+                    
+                    if ( ( $row + 1) != $num ){
+                        $col_data = explode(' ', $data[$row]);
+                    }else{
+                        $col_data = explode(';', $data[$row]);
+                    }
+                    
+                    $col_count = count($col_data);
+                    
+                    if ( $row >= $this->houseMin &&
+                        $row <= $this->houseMax
+                        ){
+                            
+                    }else{
+                        
+                        if ( $index == 0 ){
+                            echo "\t\t<th>\n";
+                        }
+                        else{
+                            echo "\t\t<td>\n";
+                        }
+                    }
+                    // Column Read
+                    for ($col = 0; $col <= $col_count; $col++){
+                        
+                        $strValue = $this->convertToUTF8( $col_data[$col] );
+                        
+                        if ( $row >= $this->houseMin &&
+                            $row <= $this->houseMax
+                            ){
+                                if ( $index != 0 && $strValue != ''){
+                                    array_push($arrData, $strValue);
+                                }
+                                if ($index == 0 && $strValue != ''){
+                                    array_push($arrTitle, $strValue);
+                                }
+                        }else{
+                            
+                            if ($index == 0)
+                            {
+                                echo "<b>";
+                                echo $strValue;
+                                echo "</b>";
+                                
+                                if ($strValue != ''){
+                                    array_push($arrHeader, $strValue);
+                                }
+                            }
+                            else{
+                                
+                                echo $strValue . "&nbsp";
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                    if ( $row >= $this->houseMin &&
+                        $row <= $this->houseMax
+                        )
+                    {}
+                    else{
+                        
+                        if ( $index == 0 ){
+                            echo "\t\t</th>\n";
+                        }
+                        else{
+                            echo "\t\t</td>\n";
+                        }
+                    }
+                    
+                }
+                echo "\t</tr>\n";
+                
+                $arrCount = sizeof($arrData);
+                
+                // 예상 시뮬레이션 출력
+                if ( $arrCount == ( $this->houseMax - $this->houseMin ) ){
+                    
+                    echo "\t<tr>\n";
+                    foreach ( $arrTitle as $arrVal ){
+                        
+                        echo "<th colspan=\"7\">";
+                        echo "<b>$arrVal</b";
+                        echo "</th>";
+                    }
+                    echo "\t</tr>\n";
+                    
+                    echo "\t<tr>\n";
+                    
+                    foreach ( $arrData as $arrVal ){
+                        
+                        echo "<td colspan=\"7\">";
+                        echo $arrVal;
+                        echo "</td>";
+                        array_pop($arrData);
+                    }
+                    
+                    echo "\t</tr>\n";
+                }
+                
+                $index++;
+            }
+            echo "</table>\n";
+            
+            fclose($handle);
+        }
+        
+    }
     
     // CSV To Viewer(School)
     function csvToSchoolPay($file){
